0a9162226e81d33e12a1bf954bde3b4bb5c304ae
NicholasA2 <89606271+NicholasA2@users.noreply.github.com>
2023-02-26 18:45:00 -0500

    Add files via upload

---
+++ app/controllers/User.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\controllers;
+
+class User extends \app\core\Controller{
+
+	public function index(){
+		if(isset($_POST['action'])){
+			$user = new \app\models\User();
+			$user = $user->getByUsername($_POST['username']);
+			if($user){
+				if(password_verify($_POST['password'], $user->password_hash)){
+					$_SESSION['user_id'] = $user->user_id;
+					header('location:/User/profile');
+				}else{
+					header('location:/User/index?error=Bad username/password combination');
+				}
+			}else{
+				header('location:/User/index?error=Bad username/password combination');
+			}
+
+		}else{
+			$this->view('User/index');
+		}
+	}
+
+	public function register(){
+			if(isset($_POST['action'])){
+				$user = new \app\models\User();
+				$usercheck = $user->getByUsername($_POST['username']);
+				if(!$usercheck){
+					$user->username= $_POST['username'];
+					$user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+					$user->insert();
+					header('location:/User/index');
+				}else{
+					header('location:/User/register?error=Username ' . $_POST['username'] . ' already in use. Choose another.');
+				}
+
+			}else{
+				$this->view('User/register');
+			}
+	}
+}
